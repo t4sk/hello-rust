b1ede5e9a698da06cb03b509b89e72b9ed40866d
t4sk <tsk.nakamura@gmail.com>
2022-09-07 10:43:07 +0900

    anzan

--- src/bin/anzan.rs
+++ src/bin/anzan.rs
@@ -4,31 +4,32 @@ use std::io;
 const NUM_RANDS: u32 = 5;
 
 fn main() {
-    let mut rng = rand::thread_rng();
-
-    let mut nums: Vec<u32> = vec![];
-    let mut answer: u32 = 0;
-    for i in 0..NUM_RANDS {
-        let r = rng.gen_range(1..10);
-        nums.push(r);
-        answer += r;
-    }
-
-    println!("{:?} {}", nums, answer);
-
-    // println!("Enter a number:");
-
     loop {
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("failed to readline");
+        let mut rng = rand::thread_rng();
 
-        let num: u32 = input.trim().parse().expect("input not an integer");
+        let mut nums: Vec<u32> = vec![];
+        let mut answer: u32 = 0;
+        for _ in 0..NUM_RANDS {
+            let r = rng.gen_range(1..10);
+            nums.push(r);
+            answer += r;
+        }
 
-        if num == answer {
-            println!("correct!");
-            return;
+        // println!("{:?} {}", nums, answer);
+        println!("{:?}", nums);
+
+        loop {
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("failed to readline");
+
+            let num: u32 = input.trim().parse().expect("input not an integer");
+
+            if num == answer {
+                println!("correct!");
+                break;
+            }
         }
     }
 }
