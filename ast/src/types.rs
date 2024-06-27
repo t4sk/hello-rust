b6b935a976f88f1a19c66e0d55f4a42b51d5b4f6
t4sk <tsk.nakamura@gmail.com>
2024-06-27 19:42:26 +0900

    serde

---
+++ ast/src/types.rs
@@ -0,0 +1,85 @@
+use serde::{Deserialize};
+
+#[derive(Debug, Deserialize)]
+pub enum NodeType {
+    SourceUnit,
+    PragmaDirective,
+    ImportDirective,
+    UsingForDirective,
+    ContractDefinition,
+    InheritanceSpecifier,
+    OverrideSpecifier,
+    IdentifierPath,
+    StructuredDocumentation,
+    VariableDeclaration,
+    Mapping,
+    ElementaryTypeName,
+    ElementaryTypeNameExpression,
+    ArrayTypeName,
+    TupleExpression,
+    FunctionDefinition,
+    ParameterList,
+    Block,
+    UncheckedBlock,
+    Continue,
+    Break,
+    Return,
+    Throw,
+    Literal,
+    Conditional,
+    Identifier,
+    IndexAccess,
+    IndexRangeAccess,
+    MemberAccess,
+    Assignment,
+    FunctionCall,
+    FunctionCallOptions,
+    FunctionTypeName,
+    NewExpression,
+    ExpressionStatement,
+    VariableDeclarationStatement,
+    IfStatement,
+    TryCatchClause,
+    UnaryOperation,
+    BinaryOperation,
+    EventDefinition,
+    ErrorDefinition,
+    EmitStatement,
+    PlaceholderStatement,
+    TryStatement,
+    RevertStatement,
+    ForStatement,
+    WhileStatement,
+    DoWhileStatement,
+    ModifierDefinition,
+    ModifierInvocation,
+    EnumDefinition,
+    EnumValue,
+    StructDefinition,
+    UserDefinedTypeName,
+    InlineAssembly,
+    YulLiteral,
+    YulTypedName,
+    YulSwitch,
+    YulCase,
+    YulFunctionCall,
+    YulExpressionStatement,
+    YulAssignment,
+    YulIdentifier,
+    YulVariableDeclaration,
+    YulBlock,
+    UserDefinedValueTypeDefinition,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Node {
+    pub id: u32,
+    pub node_type: NodeType,
+    pub nodes: Vec<Node>
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Ast {
+    pub ast: Node
+}
