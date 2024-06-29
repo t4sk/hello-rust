443bca78e325bf0d6ea846fbd151cdb5dba93db5
t4sk <tsk.nakamura@gmail.com>
2024-06-29 14:56:05 +0900

    ast

--- ast/src/main.rs
+++ ast/src/main.rs
@@ -5,46 +5,47 @@ pub mod types;
 
 use types::{
     ast::Ast,
-    node::{ContractDefintion, FunctionDefinition, Node, NodeType},
+    node::{Node, NodeType},
 };
 
 #[derive(Debug)]
 pub struct Function {
-    pub name: String
-
+    pub name: String,
 }
 
 #[derive(Debug)]
 pub struct Contract {
     pub name: String,
-    pub functions: Vec<Function>
+    pub functions: Vec<Function>,
 }
 
 fn collect(node: &Node) {
-    let c: ContractDefintion = (&node.data).into();
-    let mut contract = Contract {name: c.name, functions: vec![]};
+    // let c: ContractDefintion = serde_json::from_str(node.data.to_string()).unwrap();
+    // let mut contract = Contract {name: c.name, functions: vec![]};
 
-    for node in node.nodes.iter() {
-        if node.node_type == NodeType::FunctionDefinition {
-            let f: FunctionDefinition = (&node.data).into();
-            println!("FUNC {:#?}", f);
-            contract.functions.push(Function {name: f.name});
+    // // for node in node.nodes.iter() {
+    // //     if node.node_type == NodeType::FunctionDefinition {
+    // //         let f: FunctionDefinition = (&node.data).into();
+    // //         println!("FUNC {:#?}", f);
+    // //         contract.functions.push(Function {name: f.name});
 
-        }
-    }
+    // //     }
+    // // }
 
-    println!("HERE {:#?}", contract);
+    println!("HERE {:#?}", node);
 }
 
 fn bfs(node: &Node, node_type: NodeType, f: fn(&Node)) {
     let mut q = vec![node];
 
     while let Some(node) = q.pop() {
-        for node in node.nodes.iter() {
-            if node.node_type == node_type {
-                f(&node);
+        if let Some(nodes) = node.get_nodes() {
+            for node in nodes.iter() {
+                if node.get_type() == node_type {
+                    f(&node);
+                }
+                q.push(node);
             }
-            q.push(node);
         }
     }
 }
@@ -53,7 +54,7 @@ fn main() {
     let file_path = "tmp/ERC20.json";
     let content = fs::read_to_string(file_path).unwrap();
     let ast = serde_json::from_str::<Ast>(&content).unwrap();
-    // println!("{:#?}", ast);
+    println!("{:#?}", ast);
 
     bfs(&ast.ast, NodeType::ContractDefinition, collect);
 }
--- ast/src/types/node.rs
+++ ast/src/types/node.rs
@@ -1,7 +1,4 @@
 use serde::Deserialize;
-use serde_json::Value;
-use std::collections::HashMap;
-use std::convert::Into;
 
 #[derive(Debug, Deserialize, PartialEq)]
 pub enum NodeType {
@@ -72,51 +69,240 @@ pub enum NodeType {
     YulVariableDeclaration,
     YulBlock,
     UserDefinedValueTypeDefinition,
+    Unknown,
 }
 
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Node {
-    pub id: u32,
-    pub node_type: NodeType,
-    pub nodes: Vec<Node>,
-    #[serde(flatten)]
-    pub data: HashMap<String, Value>,
+#[serde(tag = "nodeType")]
+pub enum Node {
+    #[serde(rename_all = "camelCase")]
+    SourceUnit { id: u32, nodes: Vec<Node> },
+    #[serde(rename_all = "camelCase")]
+    PragmaDirective {},
+    #[serde(rename_all = "camelCase")]
+    ImportDirective {},
+    #[serde(rename_all = "camelCase")]
+    UsingForDirective {},
+    #[serde(rename_all = "camelCase")]
+    ContractDefinition {
+        id: u32,
+        nodes: Vec<Node>,
+        name: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    InheritanceSpecifier {},
+    #[serde(rename_all = "camelCase")]
+    OverrideSpecifier {},
+    #[serde(rename_all = "camelCase")]
+    IdentifierPath {},
+    #[serde(rename_all = "camelCase")]
+    StructuredDocumentation {},
+    #[serde(rename_all = "camelCase")]
+    VariableDeclaration {},
+    #[serde(rename_all = "camelCase")]
+    Mapping {},
+    #[serde(rename_all = "camelCase")]
+    ElementaryTypeName {},
+    #[serde(rename_all = "camelCase")]
+    ElementaryTypeNameExpression {},
+    #[serde(rename_all = "camelCase")]
+    ArrayTypeName {},
+    #[serde(rename_all = "camelCase")]
+    TupleExpression {},
+    #[serde(rename_all = "camelCase")]
+    FunctionDefinition {
+        id: u32,
+        nodes: Vec<Node>,
+        name: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    ParameterList {},
+    #[serde(rename_all = "camelCase")]
+    Block {},
+    #[serde(rename_all = "camelCase")]
+    UncheckedBlock {},
+    #[serde(rename_all = "camelCase")]
+    Continue {},
+    #[serde(rename_all = "camelCase")]
+    Break {},
+    #[serde(rename_all = "camelCase")]
+    Return {},
+    #[serde(rename_all = "camelCase")]
+    Throw {},
+    #[serde(rename_all = "camelCase")]
+    Literal {},
+    #[serde(rename_all = "camelCase")]
+    Conditional {},
+    #[serde(rename_all = "camelCase")]
+    Identifier {},
+    #[serde(rename_all = "camelCase")]
+    IndexAccess {},
+    #[serde(rename_all = "camelCase")]
+    IndexRangeAccess {},
+    #[serde(rename_all = "camelCase")]
+    MemberAccess {},
+    #[serde(rename_all = "camelCase")]
+    Assignment {},
+    #[serde(rename_all = "camelCase")]
+    FunctionCall {},
+    #[serde(rename_all = "camelCase")]
+    FunctionCallOptions {},
+    #[serde(rename_all = "camelCase")]
+    FunctionTypeName {},
+    #[serde(rename_all = "camelCase")]
+    NewExpression {},
+    #[serde(rename_all = "camelCase")]
+    ExpressionStatement {},
+    #[serde(rename_all = "camelCase")]
+    VariableDeclarationStatement {},
+    #[serde(rename_all = "camelCase")]
+    IfStatement {},
+    #[serde(rename_all = "camelCase")]
+    TryCatchClause {},
+    #[serde(rename_all = "camelCase")]
+    UnaryOperation {},
+    #[serde(rename_all = "camelCase")]
+    BinaryOperation {},
+    #[serde(rename_all = "camelCase")]
+    EventDefinition {},
+    #[serde(rename_all = "camelCase")]
+    ErrorDefinition {},
+    #[serde(rename_all = "camelCase")]
+    EmitStatement {},
+    #[serde(rename_all = "camelCase")]
+    PlaceholderStatement {},
+    #[serde(rename_all = "camelCase")]
+    TryStatement {},
+    #[serde(rename_all = "camelCase")]
+    RevertStatement {},
+    #[serde(rename_all = "camelCase")]
+    ForStatement {},
+    #[serde(rename_all = "camelCase")]
+    WhileStatement {},
+    #[serde(rename_all = "camelCase")]
+    DoWhileStatement {},
+    #[serde(rename_all = "camelCase")]
+    ModifierDefinition {},
+    #[serde(rename_all = "camelCase")]
+    ModifierInvocation {},
+    #[serde(rename_all = "camelCase")]
+    EnumDefinition {},
+    #[serde(rename_all = "camelCase")]
+    EnumValue {},
+    #[serde(rename_all = "camelCase")]
+    StructDefinition {},
+    #[serde(rename_all = "camelCase")]
+    UserDefinedTypeName {},
+    #[serde(rename_all = "camelCase")]
+    InlineAssembly {},
+    #[serde(rename_all = "camelCase")]
+    YulLiteral {},
+    #[serde(rename_all = "camelCase")]
+    YulTypedName {},
+    #[serde(rename_all = "camelCase")]
+    YulSwitch {},
+    #[serde(rename_all = "camelCase")]
+    YulCase {},
+    #[serde(rename_all = "camelCase")]
+    YulFunctionCall {},
+    #[serde(rename_all = "camelCase")]
+    YulExpressionStatement {},
+    #[serde(rename_all = "camelCase")]
+    YulAssignment {},
+    #[serde(rename_all = "camelCase")]
+    YulIdentifier {},
+    #[serde(rename_all = "camelCase")]
+    YulVariableDeclaration {},
+    #[serde(rename_all = "camelCase")]
+    YulBlock {},
+    #[serde(rename_all = "camelCase")]
+    UserDefinedValueTypeDefinition {},
+    #[serde(rename_all = "camelCase")]
+    #[serde(untagged)]
+    Unknown { id: u32, nodes: Vec<Node> },
 }
 
-#[derive(Debug)]
-pub struct ContractDefintion {
-    pub name: String,
-}
-
-impl Into<ContractDefintion> for &HashMap<String, Value> {
-    fn into(self) -> ContractDefintion {
-        ContractDefintion {
-            name: self.get("name").unwrap().as_str().unwrap().to_string(),
+impl Node {
+    pub fn get_type(&self) -> NodeType {
+        match self {
+            Node::SourceUnit { .. } => NodeType::SourceUnit,
+            Node::PragmaDirective { .. } => NodeType::PragmaDirective,
+            Node::ImportDirective { .. } => NodeType::ImportDirective,
+            Node::UsingForDirective { .. } => NodeType::UsingForDirective,
+            Node::ContractDefinition { .. } => NodeType::ContractDefinition,
+            Node::InheritanceSpecifier { .. } => NodeType::InheritanceSpecifier,
+            Node::OverrideSpecifier { .. } => NodeType::OverrideSpecifier,
+            Node::IdentifierPath { .. } => NodeType::IdentifierPath,
+            Node::StructuredDocumentation { .. } => NodeType::StructuredDocumentation,
+            Node::VariableDeclaration { .. } => NodeType::VariableDeclaration,
+            Node::Mapping { .. } => NodeType::Mapping,
+            Node::ElementaryTypeName { .. } => NodeType::ElementaryTypeName,
+            Node::ElementaryTypeNameExpression { .. } => NodeType::ElementaryTypeNameExpression,
+            Node::ArrayTypeName { .. } => NodeType::ArrayTypeName,
+            Node::TupleExpression { .. } => NodeType::TupleExpression,
+            Node::FunctionDefinition { .. } => NodeType::FunctionDefinition,
+            Node::ParameterList { .. } => NodeType::ParameterList,
+            Node::Block { .. } => NodeType::Block,
+            Node::UncheckedBlock { .. } => NodeType::UncheckedBlock,
+            Node::Continue { .. } => NodeType::Continue,
+            Node::Break { .. } => NodeType::Break,
+            Node::Return { .. } => NodeType::Return,
+            Node::Throw { .. } => NodeType::Throw,
+            Node::Literal { .. } => NodeType::Literal,
+            Node::Conditional { .. } => NodeType::Conditional,
+            Node::Identifier { .. } => NodeType::Identifier,
+            Node::IndexAccess { .. } => NodeType::IndexAccess,
+            Node::IndexRangeAccess { .. } => NodeType::IndexRangeAccess,
+            Node::MemberAccess { .. } => NodeType::MemberAccess,
+            Node::Assignment { .. } => NodeType::Assignment,
+            Node::FunctionCall { .. } => NodeType::FunctionCall,
+            Node::FunctionCallOptions { .. } => NodeType::FunctionCallOptions,
+            Node::FunctionTypeName { .. } => NodeType::FunctionTypeName,
+            Node::NewExpression { .. } => NodeType::NewExpression,
+            Node::ExpressionStatement { .. } => NodeType::ExpressionStatement,
+            Node::VariableDeclarationStatement { .. } => NodeType::VariableDeclarationStatement,
+            Node::IfStatement { .. } => NodeType::IfStatement,
+            Node::TryCatchClause { .. } => NodeType::TryCatchClause,
+            Node::UnaryOperation { .. } => NodeType::UnaryOperation,
+            Node::BinaryOperation { .. } => NodeType::BinaryOperation,
+            Node::EventDefinition { .. } => NodeType::EventDefinition,
+            Node::ErrorDefinition { .. } => NodeType::ErrorDefinition,
+            Node::EmitStatement { .. } => NodeType::EmitStatement,
+            Node::PlaceholderStatement { .. } => NodeType::PlaceholderStatement,
+            Node::TryStatement { .. } => NodeType::TryStatement,
+            Node::RevertStatement { .. } => NodeType::RevertStatement,
+            Node::ForStatement { .. } => NodeType::ForStatement,
+            Node::WhileStatement { .. } => NodeType::WhileStatement,
+            Node::DoWhileStatement { .. } => NodeType::DoWhileStatement,
+            Node::ModifierDefinition { .. } => NodeType::ModifierDefinition,
+            Node::ModifierInvocation { .. } => NodeType::ModifierInvocation,
+            Node::EnumDefinition { .. } => NodeType::EnumDefinition,
+            Node::EnumValue { .. } => NodeType::EnumValue,
+            Node::StructDefinition { .. } => NodeType::StructDefinition,
+            Node::UserDefinedTypeName { .. } => NodeType::UserDefinedTypeName,
+            Node::InlineAssembly { .. } => NodeType::InlineAssembly,
+            Node::YulLiteral { .. } => NodeType::YulLiteral,
+            Node::YulTypedName { .. } => NodeType::YulTypedName,
+            Node::YulSwitch { .. } => NodeType::YulSwitch,
+            Node::YulCase { .. } => NodeType::YulCase,
+            Node::YulFunctionCall { .. } => NodeType::YulFunctionCall,
+            Node::YulExpressionStatement { .. } => NodeType::YulExpressionStatement,
+            Node::YulAssignment { .. } => NodeType::YulAssignment,
+            Node::YulIdentifier { .. } => NodeType::YulIdentifier,
+            Node::YulVariableDeclaration { .. } => NodeType::YulVariableDeclaration,
+            Node::YulBlock { .. } => NodeType::YulBlock,
+            Node::UserDefinedValueTypeDefinition { .. } => NodeType::UserDefinedValueTypeDefinition,
+            Node::Unknown { .. } => NodeType::Unknown,
         }
     }
-}
 
-#[derive(Debug, Deserialize, PartialEq)]
-pub enum FunctionKind {
-    Constructor,
-    Function,
-    Receive,
-    Fallback,
-    FreeFunction,
-}
-
-
-#[derive(Debug)]
-pub struct FunctionDefinition {
-    pub name: String,
-    // pub kind: FunctionKind
-}
-
-impl Into<FunctionDefinition> for &HashMap<String, Value> {
-    fn into(self) -> FunctionDefinition {
-        FunctionDefinition {
-            name: self.get("name").unwrap().as_str().unwrap().to_string(),
+    pub fn get_nodes(&self) -> Option<&Vec<Node>> {
+        match self {
+            Node::SourceUnit { nodes, .. } => Some(&nodes),
+            Node::ContractDefinition { nodes, .. } => Some(&nodes),
+            Node::FunctionDefinition { nodes, .. } => Some(&nodes),
+            Node::Unknown { nodes, .. } => Some(&nodes),
+            _ => None,
         }
     }
 }
