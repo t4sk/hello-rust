628af27908213545d61f89fe277ac1f5d281d514
t4sk <tsk.nakamura@gmail.com>
2024-06-30 10:36:17 +0900

    method

--- basic/src/bin/method.rs
+++ basic/src/bin/method.rs
@@ -4,12 +4,21 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
     fn area(&self) -> u32 {
         self.width * self.height
     }
 }
 
 fn main() {
+    let square = Rectangle::square(10);
+
     let rect = Rectangle {
         width: 5,
         height: 3,
