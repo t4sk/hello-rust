c3b17016ed81c0d853d40b7d27e752a9110f8808
t4sk <tsk.nakamura@gmail.com>
2024-07-25 19:26:14 +0900

    closures

---
+++ basic/src/bin/closure_out.rs
@@ -0,0 +1,32 @@
+// Return closure as output
+
+fn create_fn() -> impl Fn() {
+    let text = "hello".to_string();
+    move || println!("fn {}", text)
+}
+
+fn create_fn_mut() -> impl FnMut() {
+    let text = "hello".to_string();
+    move || println!("fn mut {}", text)
+}
+
+fn create_fn_once() -> impl FnOnce() {
+    let text = "hello".to_string();
+    move || println!("fn once {}", text)
+}
+
+fn main() {
+    let f = create_fn();
+    f();
+    f();
+
+    // TODO: why mut on f?
+    let mut f = create_fn_mut();
+    f();
+    f();
+
+    let f = create_fn_once();
+    f();
+    // Note: cannot call again
+    // f();
+}
