dfba05fbe6aef5ed1eb122aed02a460967b4f9e8
t4sk <tsk.nakamura@gmail.com>
2024-08-01 20:47:12 +0900

    rc

--- basic/src/bin/smart_pointers.rs
+++ basic/src/bin/smart_pointers.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 // Smart pointers, on the other hand, are data structures that act like a
 // pointer but also have additional metadata and capabilities
 
@@ -10,6 +11,17 @@ struct Tree {
     left: Option<Box<Tree>>,
     right: Option<Box<Tree>>,
 }
+
+// RC<T> - reference count
+// enable multiple ownership explicitly by using the Rust type Rc<T>,
+use std::rc::Rc;
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
 
 fn main() {
     // Box<T>
@@ -32,4 +44,20 @@ fn main() {
     };
 
     println!("{:#?}", tree);
+
+    // RC<T>
+    // Rc::clone doesn't make deep copy
+    // Cloning increases reference count
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("{}", Rc::strong_count(&a));
+
+    let b = Cons(3, Rc::clone(&a));
+    println!("{}", Rc::strong_count(&a));
+
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("{}", Rc::strong_count(&a));
+    }
+
+    println!("{}", Rc::strong_count(&a));
 }
