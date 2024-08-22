8ff857d90debf9ca20698619ed2b9508586ddee6
t4sk <tsk.nakamura@gmail.com>
2024-08-22 10:51:06 +0900

    ast

--- ast/src/main.rs
+++ ast/src/main.rs
@@ -1,7 +1,113 @@
 use std::fs;
+use serde_json::Value;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum Mutability {
+    Mutable,
+    Immutable,
+    Constant,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum StateMutability {
+    Payable,
+    NonPayable,
+    Pure,
+    View,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum Visibility {
+    Private,
+    Internal,
+    Public,
+    External,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum StorageLocation {
+    Calldata,
+    Default,
+    Memory,
+    Storage,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub enum FunctionKind {
+    Constructor,
+    Receive,
+    Fallback,
+    FreeFunction,
+    Function,
+}
+
+// AST //
+#[derive(Debug, Deserialize)]
+pub struct Ast {
+    // pub ast: Value
+    pub ast: Node
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "nodeType")]
+pub enum Node {
+    ContractDefinition(ContractDefinition),
+    VariableDeclaration(VariableDeclaration),
+    FunctionDefinition(FunctionDefinition),
+    #[serde(untagged)]
+    Unknown {
+        id: u32,
+        nodes: Vec<Node>,
+    },
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ContractDefinition {
+    pub id: u32,
+    pub src: String,
+    pub name: String,
+    pub nodes: Vec<Node>
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct VariableDeclaration {
+    pub id: u32,
+    pub src: String,
+    pub constant: bool,
+    pub function_selector: Option<String>,
+    pub mutability: Mutability,
+    pub name: String,
+    pub state_variable: bool,
+    pub storage_location: StorageLocation,
+    pub visibility: Visibility,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FunctionDefinition {
+    pub id: u32,
+    pub src: String,
+    pub nodes: Vec<Node>,
+    pub kind: FunctionKind,
+    pub name: String,
+    pub visibility: Visibility,
+    pub state_mutability: StateMutability,
+    pub function_selector: Option<String>,
+    // TODO: block, parameters
+}
 
 fn main() {
     let file_path = "tmp/ERC20.json";
     let json = fs::read_to_string(file_path).unwrap();
-    println!("{:#?}", json);
+    let ast = serde_json::from_str::<Ast>(&json).unwrap();
+    println!("{:#?}", ast);
 }
