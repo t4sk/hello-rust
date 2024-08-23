ce3281652a16700d5470120901646893d032cbb3
t4sk <tsk.nakamura@gmail.com>
2024-08-23 11:22:38 +0900

    ast

--- ast/src/main.rs
+++ ast/src/main.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use serde_json::Value;
+use std::fs;
 
 use serde::Deserialize;
 
@@ -52,7 +52,7 @@ pub enum FunctionKind {
 #[derive(Debug, Deserialize)]
 pub struct Ast {
     // pub ast: Value
-    pub ast: Node
+    pub ast: Node,
 }
 
 #[derive(Debug, Deserialize)]
@@ -74,7 +74,182 @@ pub struct ContractDefinition {
     pub id: u32,
     pub src: String,
     pub name: String,
-    pub nodes: Vec<Node>
+    pub nodes: Vec<Node>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ExpressionStatement {
+    pub id: u32,
+    pub src: String,
+    pub expression: Box<Expression>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "nodeType")]
+pub enum Statement {
+    Block,
+    Break,
+    Continue,
+    DoWhileStatement,
+    EmitStatement,
+    ExpressionStatement(ExpressionStatement),
+    ForStatement,
+    IfStatement,
+    InlineAssembly,
+    PlaceholderStatement,
+    Return,
+    RevertStatement,
+    TryStatement,
+    UncheckedBlock,
+    VariableDeclarationStatement,
+    WhileStatement,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Block {
+    pub id: u32,
+    pub src: String,
+    pub statements: Option<Vec<Statement>>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum FunctionCallKind {
+    FunctionCall,
+    TypeConversion,
+    StructConstructorCall,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FunctionCall {
+    pub id: u32,
+    pub src: String,
+    pub argument_types: Option<Vec<TypeDescriptions>>,
+    pub arguments: Vec<Expression>,
+    pub expression: Vec<Expression>,
+    pub is_constant: bool,
+    pub is_l_value: bool,
+    pub is_pure: bool,
+    pub kind: FunctionCallKind,
+    pub l_value_requested: bool,
+    pub name_locations: Option<Vec<String>>,
+    pub names: Vec<String>,
+    pub try_call: bool,
+    // pub type_descriptions: TypeDescriptions,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FunctionCallOptions {
+    pub id: u32,
+    pub src: String,
+    pub argument_types: Option<Vec<TypeDescriptions>>,
+    pub expression: Box<Expression>,
+    pub is_constant: bool,
+    pub is_l_value: bool,
+    pub is_pure: bool,
+    pub l_value_requested: bool,
+    pub names: Vec<String>,
+    pub options: Vec<Expression>,
+    pub type_descriptions: TypeDescriptions,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "nodeType")]
+pub enum Expression {
+    Assignment,
+    BinaryOperation,
+    Conditional,
+    ElementaryTypeNameExpression,
+    FunctionCall(FunctionCall),
+    FunctionCallOptions(FunctionCallOptions),
+    Identifier,
+    IndexAccess,
+    IndexRangeAccess,
+    Literal,
+    MemberAccess,
+    NewExpression,
+    TupleExpression,
+    UnaryOperation,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ArrayTypeName {
+    pub id: u32,
+    pub src: String,
+    pub base_type: Box<TypeName>,
+    pub length: Option<Expression>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ElementaryTypeName {
+    pub id: u32,
+    pub src: String,
+    pub name: String,
+    pub state_mutability: Option<StateMutability>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FunctionTypeName {
+    pub id: u32,
+    pub src: String,
+    pub state_mutability: StateMutability,
+    pub visibility: Visibility,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Mapping {
+    pub id: u32,
+    pub src: String,
+    pub key_name: Option<String>,
+    pub key_name_location: Option<String>,
+    pub key_type: Box<TypeName>,
+    pub value_name: Option<String>,
+    pub value_name_location: Option<String>,
+    pub value_type: Box<TypeName>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct IdentifierPath {
+    pub id: u32,
+    pub src: String,
+    pub name: String,
+    pub referenced_declaration: Option<u32>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UserDefinedTypeName {
+    pub id: u32,
+    pub src: String,
+    pub name: Option<String>,
+    pub path_node: Option<IdentifierPath>,
+    pub reference_declaration: u32,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "nodeType")]
+pub enum TypeName {
+    ArrayTypeName(ArrayTypeName),
+    ElementaryTypeName(ElementaryTypeName),
+    FunctionTypeName(FunctionTypeName),
+    Mapping(Mapping),
+    UserDefinedTypeName(UserDefinedTypeName),
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TypeDescriptions {
+    pub type_identifier: Option<String>,
+    pub type_string: Option<String>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -89,6 +264,15 @@ pub struct VariableDeclaration {
     pub state_variable: bool,
     pub storage_location: StorageLocation,
     pub visibility: Visibility,
+    pub type_name: Option<TypeName>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ParameterList {
+    pub id: u32,
+    pub src: String,
+    pub parameters: Vec<VariableDeclaration>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -102,7 +286,9 @@ pub struct FunctionDefinition {
     pub visibility: Visibility,
     pub state_mutability: StateMutability,
     pub function_selector: Option<String>,
-    // TODO: block, parameters
+    pub body: Option<Block>,
+    pub parameters: ParameterList,
+    pub return_parameters: ParameterList,
 }
 
 fn main() {
