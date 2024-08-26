642d87c7e615dac7945cfd2c69f2fd86742fa994
t4sk <tsk.nakamura@gmail.com>
2024-08-26 21:09:02 +0900

    start spinner

--- spinner/src/main.rs
+++ spinner/src/main.rs
@@ -1,3 +1,23 @@
+// use std::io::{self, Write};
+use std::{
+    io::{stderr, stdout, Result, Write},
+    time::Instant,
+};
+
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    let frames = vec!["-", "\\", "|", "/"];
+
+    let mut stream = std::io::stdout();
+
+    loop {
+        for f in frames.iter() {
+            // print!("\r{}", f);
+            // io::stdout().flush().unwrap();
+            write!(stream, "\r{}", f).unwrap();
+            stream.flush().unwrap();
+            thread::sleep(time::Duration::from_millis(200));
+        }
+    }
 }
