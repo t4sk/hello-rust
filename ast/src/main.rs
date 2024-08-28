0a04345580737e639af96d5bdbd4ae9c9601b76c
t4sk <tsk.nakamura@gmail.com>
2024-08-28 11:09:27 +0900

    inner

--- ast/src/main.rs
+++ ast/src/main.rs
@@ -5,66 +5,79 @@ pub mod types;
 use types::Ast;
 
 fn main() {
-    let file_path = "tmp/Pot.json";
+    let file_path = "tmp/Ast.json";
     let json = fs::read_to_string(file_path).unwrap();
     let ast = serde_json::from_str::<Ast>(&json).unwrap();
 
     // println!("{:#?}", ast);
     // return;
 
+    // FunctionDefinition
+    // -> name
+    // -> statements
+    //    -> FunctionCall
+    //       -> expression
+    //          -> Identifier
+    //             -> name
+    //
+
     for node in ast.ast.nodes.iter() {
-        match node {
-            types::Node::ContractDefinition(contract_def) => {
-                println!("{}", contract_def.name);
-                for node in contract_def.nodes.iter() {
-                    match node {
-                        types::Node::VariableDeclaration(var_dec) => {
-                            if var_dec.state_variable {
-                                println!("{}", var_dec.name);
-                            }
+        if let types::Node::ContractDefinition(contract_def) = node {
+            println!("{}", contract_def.name);
+
+            for node in contract_def.nodes.iter() {
+                match node {
+                    types::Node::VariableDeclaration(var_dec) => {
+                        if var_dec.state_variable {
+                            println!("{}", var_dec.name);
                         }
-                        types::Node::FunctionDefinition(func_def) => {
-                            println!("- {}", func_def.name);
-                            if let Some(body) = &func_def.body {
-                                if let Some(statements) = &body.statements {
-                                    for s in statements.iter() {
-                                        match s {
-                                            types::Statement::ExpressionStatement(
-                                                exp_statement,
-                                            ) => {
-                                                match *exp_statement.expression.clone() {
-                                                    types::Expression::FunctionCall(func_call) => {
-                                                        // func_call.expression -> MemberAccess -> member_name
-                                                        //                                      -> expression -> Identifier -> name
-                                                        match *func_call.expression {
-                                                            types::Expression::MemberAccess(
-                                                                mem_acc,
-                                                            ) => {
-                                                                let func = mem_acc.member_name;
-                                                                match *mem_acc.expression {
-                                                                    types::Expression::Identifier(id) => {
-                                                                        println!("-- {}.{}", id.name, func);
-                                                                    },
-                                                                    _ => ()
+                    }
+                    types::Node::FunctionDefinition(func_def) => {
+                        println!("- {}", func_def.name);
+                        if let Some(body) = &func_def.body {
+                            if let Some(statements) = &body.statements {
+                                for s in statements.iter() {
+                                    match s {
+                                        types::Statement::ExpressionStatement(exp_statement) => {
+                                            match *exp_statement.expression.clone() {
+                                                types::Expression::FunctionCall(func_call) => {
+                                                    // func_call.expression -> MemberAccess -> member_name
+                                                    //                                      -> expression -> Identifier -> name
+                                                    match *func_call.expression {
+                                                        types::Expression::MemberAccess(
+                                                            mem_acc,
+                                                        ) => {
+                                                            let func = mem_acc.member_name;
+                                                            match *mem_acc.expression {
+                                                                types::Expression::Identifier(
+                                                                    id,
+                                                                ) => {
+                                                                    println!(
+                                                                        "-- {}.{}",
+                                                                        id.name, func
+                                                                    );
                                                                 }
+                                                                _ => (),
                                                             }
-                                                            _ => (),
                                                         }
+                                                        types::Expression::Identifier(id) => {
+                                                            println!("-- {}", id.name);
+                                                        }
+                                                        _ => (),
                                                     }
-                                                    _ => (),
                                                 }
+                                                _ => (),
                                             }
-                                            _ => (),
                                         }
+                                        _ => (),
                                     }
                                 }
                             }
                         }
-                        _ => (),
                     }
+                    _ => (),
                 }
             }
-            _ => (),
         }
     }
 }
