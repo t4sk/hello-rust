739b2d369bfdb632f9576e85c3276baf9335f651
t4sk <tsk.nakamura@gmail.com>
2024-09-05 22:58:20 +0900

    spinner

---
+++ spinner/README.md
@@ -0,0 +1,3 @@
+```shell
+cargo run
+```
--- spinner/src/main.rs
+++ spinner/src/main.rs
@@ -5,12 +5,15 @@ use std::sync::mpsc::{channel, Receiver, Sender, TryRecvError};
 
 pub struct Spinner {
     tx: Sender<Option<String>>,
+    // JoinHandle::join method takes ownership, so wrap it in an Option
+    // JoinHandle::join blocks until the corresponding thread is done executing
     join_handle: Option<thread::JoinHandle<()>>,
 }
 
+// Called when Spinner goes out of scope
 impl Drop for Spinner {
     fn drop(&mut self) {
-        self.tx.send(None).unwrap();
+        self.tx.send(Some("drop".to_string())).unwrap();
         self.join_handle.take().unwrap().join().unwrap();
     }
 }
@@ -40,6 +43,7 @@ impl Spinner {
                         return;
                     }
 
+                    // Write to stdout stream
                     write!(stream, "\r{}", f).unwrap();
                     stream.flush().unwrap();
 
@@ -55,13 +59,30 @@ impl Spinner {
     }
 
     pub fn stop(&mut self) {
-        self.tx.send(Some(String::from("stopped"))).unwrap();
+        self.tx.send(Some(String::from("stop"))).unwrap();
     }
 }
 
 fn main() {
     let mut spinner = Spinner::start();
-    thread::sleep(time::Duration::from_secs(3));
-    // Spinner is dropped here so no need to call stop
+    thread::sleep(time::Duration::from_secs(1));
+    // Spinner is dropped after main, so no need to call stop here
     spinner.stop();
 }
+
+// Thread example
+/*
+fn main() {
+    println!("main start");
+
+    let join_handle = thread::spawn(|| {
+        println!("sleep start");
+        thread::sleep(time::Duration::from_secs(1));
+        println!("sleep end");
+    });
+
+    join_handle.join().unwrap();
+
+    println!("main end");
+}
+*/
