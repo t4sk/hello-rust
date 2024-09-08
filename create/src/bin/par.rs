88b0c416054341943ce5a5ae0aa5c19aef73af08
t4sk <tsk.nakamura@gmail.com>
2024-09-08 19:01:29 +0900

    par

--- create/src/bin/par.rs
+++ create/src/bin/par.rs
@@ -1,6 +1,9 @@
 use std::env;
+use std::sync::mpsc::channel;
 use std::thread;
 use std::thread::JoinHandle;
+
+use ethers::abi::Address;
 
 use create::{calc_contract_addr, parse_args};
 
@@ -9,6 +12,9 @@ const MAX_THREADS: u64 = 5;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let (deployer_addr, start, end) = parse_args(args);
+
+    // Channel to send and recieve results from threads
+    let (tx, rx) = channel();
 
     println!("Deployer: {:?}", deployer_addr);
     println!("nonce | contract address");
@@ -21,6 +27,8 @@ fn main() {
 
     let mut handles: Vec<JoinHandle<()>> = Vec::new();
     for i in 0..MAX_THREADS {
+        let tx = tx.clone();
+
         let handle = thread::spawn(move || {
             let start = i * n_per_thread;
             let mut end = (i + 1) * n_per_thread;
@@ -30,14 +38,26 @@ fn main() {
 
             for j in start..end {
                 let contract_addr = calc_contract_addr(deployer_addr, j);
-                // TODO: store in vec and sort
-                println!("{:?}, {:?}", j, contract_addr);
+                tx.send((j, contract_addr)).unwrap();
             }
         });
         handles.push(handle);
     }
 
+    // Drop the original sender to close channel
+    drop(tx);
+
+    // Collect results
+    let mut addrs: Vec<(u64, Address)> = Vec::new();
+    for msg in rx {
+        addrs.push(msg);
+    }
+
     for h in handles {
         h.join().unwrap();
+    }
+
+    for (i, addr) in addrs {
+        println!("{}, {}", i, addr);
     }
 }
