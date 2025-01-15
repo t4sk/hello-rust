ead944517ce8dd4a164c458d362c2034553d2068
t4sk <tsk.nakamura@gmail.com>
2025-01-15 19:04:13 +0900

    box dyn

--- src/bin/box_dyn_error.rs
+++ src/bin/box_dyn_error.rs
@@ -1,5 +1,47 @@
 #![allow(unused)]
 
+// Implementing std::error::Error for MathError and ParseError
+#[derive(Debug)]
+enum MathError {
+    DivByZero,
+}
+
+#[derive(Debug)]
+enum ParseError {
+    InvalidInt,
+}
+
+impl std::fmt::Display for MathError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "math error")
+    }
+}
+
+impl std::error::Error for MathError {}
+
+impl std::fmt::Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "parse error")
+    }
+}
+
+impl std::error::Error for ParseError {}
+
+fn f1() -> Result<u32, MathError> {
+    Err(MathError::DivByZero)
+}
+
+fn f2() -> Result<u32, ParseError> {
+    Err(ParseError::InvalidInt)
+}
+
+fn f3() -> Result<(), Box<dyn std::error::Error>> {
+    f1()?;
+    f2()?;
+    Ok(())
+}
+
+// Practical example
 use std::env;
 use std::fs::File;
 use std::io::Read;
